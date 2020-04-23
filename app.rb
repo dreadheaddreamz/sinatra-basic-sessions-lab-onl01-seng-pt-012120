2d6f8ee1f3c2b8fd61bcb866cee88d4dbcd05d83
James Batsell <jamesbatsell05@gmail.com>
2020-04-23 23:06:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,15 @@ class App < Sinatra::Base
     set :sessions_secret, "James"
   end
   
-  
-  
-  
-  
+   get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @item = params[:item]
+    session["item"] = @item
+    @session = session
+    erb :checkout
+  end
+
 end
